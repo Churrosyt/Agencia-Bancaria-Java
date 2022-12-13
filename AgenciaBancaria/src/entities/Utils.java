87317511c6ffa784c59f82b5ec8c318852ea2203
Churros <klebersonluiz5@gmail.com>
2022-12-13 18:58:32 -0300

    Finish

--- AgenciaBancaria/src/entities/Utils.java
+++ AgenciaBancaria/src/entities/Utils.java
@@ -2,14 +2,20 @@ package entities;
 
 import java.text.DecimalFormat;
 import java.text.NumberFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class Utils {
-	
-	static NumberFormat formatandoValores = new DecimalFormat("R$ #,##0.00");
-	
-	public static String doubleToString(Double valor) {
-		return formatandoValores.format(valor);
-	}
-	
+
+    static NumberFormat formatandoNumeros = new DecimalFormat("R$ #,##0.00");
+    static SimpleDateFormat formatandoData = new SimpleDateFormat("dd/MM/yyyy");
+
+    public static String dateToString(Date data) {
+        return Utils.formatandoData.format(data);
+    }
+
+    public static String doubleToString(Double valor) {
+        return Utils.formatandoNumeros.format(valor);
+    }
 
 }
